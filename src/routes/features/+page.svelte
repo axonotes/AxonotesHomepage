e2abca73aa4ea55f9557c7df9b01b0c28e8a82f1
Oliver Seifert <github.staging362@passmail.net>
2025-05-18 00:31:08 +0200

    feat: added features front page

--- src/routes/features/+page.svelte
+++ src/routes/features/+page.svelte
@@ -4,6 +4,20 @@
     export let data: PageData;
 </script>
 
-{#each data.features as feature}
-    <p>{feature.name}</p>
-{/each}
+<div class="text-center mb-8">
+    <h2 class="text-3xl">Axonotes Features</h2>
+    <p class="font-light">This page shows features that are planed for Axonotes</p>
+</div>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    {#each data.features as feature}
+        <a href={feature.url} class="p-4 border rounded-md border-primary-500">
+            <p>{feature.name}</p>
+            <div class="w-full flex justify-end">
+                <span class="badge">
+                    👍 {feature.upvotes}
+                </span>
+            </div>
+        </a>
+    {/each}
+</div>
