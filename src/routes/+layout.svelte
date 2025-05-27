0a7f322c8f3c2290f2323a16252f414a72170109
Oliver Seifert <github.staging362@passmail.net>
2025-05-27 17:42:22 +0200

    feat: updated Navbar and Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import '../app.css';
+    import '../app.css';
     import Navbar from "$lib/components/Navbar.svelte";
 
-	let { children } = $props();
+    let {children} = $props();
 </script>
 
-<div class="flex min-h-screen flex-col bg-gradient-to-br from-primary-700/30 to-transparent dark:from-transparent dark:to-primary-700/20">
+<div class="flex min-h-screen flex-col bg-gradient-to-br from-primary-700/10 to-transparent dark:from-transparent dark:to-primary-700/20">
     <header class="sticky top-0 z-50 w-full">
-        <Navbar />
+        <Navbar/>
     </header>
 
     <main class="flex-grow container mx-auto p-4 sm:p-6 lg:p-8">
