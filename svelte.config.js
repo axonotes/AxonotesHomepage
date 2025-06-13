34f963fd79cdfa429315a79500082694085c1349
Oliver Seifert <github.staging362@passmail.net>
2025-06-13 15:39:54 +0200

    feat: svx support

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,42 @@
 import adapter from "svelte-adapter-bun";
 import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
+import {mdsvex} from "mdsvex";
+import {createHighlighter} from "shiki";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+    extensions: [".svelte", ".svx"],
+
+    preprocess: [
+        vitePreprocess(),
+        mdsvex({
+            extensions: [".svx"],
+            highlight: {
+                highlighter: async (code, lang = "bash") => {
+                    const highlighter = await createHighlighter({
+                        themes: ["monokai"],
+                        langs: [
+                            "javascript",
+                            "typescript",
+                            "css",
+                            "html",
+                            "svelte",
+                            "rust",
+                            "markdown",
+                            "json",
+                            "toml",
+                        ],
+                    });
+
+                    const html = highlighter.codeToHtml(code, {
+                        lang,
+                        theme: "monokai",
+                    });
+                    return `{@html \`${html}\` }`;
+                },
+            },
+        }),
+    ],
 
     kit: {
         adapter: adapter({
