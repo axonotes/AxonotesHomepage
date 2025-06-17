b89a07d9e06f98a0b0de009a26ef94dba58c0202
Oliver Seifert <github.staging362@passmail.net>
2025-06-17 22:15:18 +0200

    feat: removed terms of service and changed route of imprint

--- src/routes/legal/privacy-policy/+page.svelte
+++ src/routes/legal/privacy-policy/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-        class="prose prose-axonotes dark:prose-invert container mx-auto px-4 py-6 sm:py-8 lg:py-12"
+    class="prose prose-axonotes dark:prose-invert container mx-auto px-4 py-6 sm:py-8 lg:py-12"
 >
     <PrivacyPolicy />
 </div>
